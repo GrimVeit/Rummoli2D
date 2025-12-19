88ff128c746ddd65afedbc87db2efadcae0e27b5
Denis Khasanov <denishasanov828@gmail.com>
2025-12-19 22:25:25 +0500

    0.0.2.2.1

---
+++ Rummoli2D/Assets/Scripts/MachineState/Game/States_Game/StartingBalanceState_Game.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingBalanceState_Game : IState
+{
+    public void EnterState()
+    {
+
+    }
+
+    public void ExitState()
+    {
+
+    }
+}
